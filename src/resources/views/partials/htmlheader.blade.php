411889f94a8f5a5b8514ec82b661f177de145768
Sergi Tur <sergiturbadenas@gmail.com>
2015-06-03 18:41:12 +0200

    Use assets to point to assets URL. Avoid errors when using Apache URL aliases

--- src/resources/views/partials/htmlheader.blade.php
+++ src/resources/views/partials/htmlheader.blade.php
@@ -9,14 +9,14 @@
     <!-- Ionicons -->
     <link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css" />
     <!-- Theme style -->
-    <link href="/css/AdminLTE.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/css/AdminLTE.css') }}" rel="stylesheet" type="text/css" />
     <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
           page. However, you can choose any other skin. Make sure you
           apply the skin class to the body tag so the changes take effect.
     -->
-    <link href="/css/skins/skin-blue.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/css/skins/skin-blue.css') }}" rel="stylesheet" type="text/css" />
     <!-- iCheck -->
-    <link href="/plugins/iCheck/square/blue.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/plugins/iCheck/square/blue.css') }}" rel="stylesheet" type="text/css" />
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
--- src/resources/views/partials/scripts.blade.php
+++ src/resources/views/partials/scripts.blade.php
@@ -1,11 +1,11 @@
 <!-- REQUIRED JS SCRIPTS -->
 
 <!-- jQuery 2.1.4 -->
-<script src="/plugins/jQuery/jQuery-2.1.4.min.js"></script>
+<script src="{{ asset('/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
 <!-- Bootstrap 3.3.2 JS -->
-<script src="/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="{{ asset('/js/bootstrap.min.js') }}" type="text/javascript"></script>
 <!-- AdminLTE App -->
-<script src="/js/app.min.js" type="text/javascript"></script>
+<script src="{{ asset('/js/app.min.js') }}" type="text/javascript"></script>
 
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
       Both of these plugins are recommended to enhance the
