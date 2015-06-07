bfaeb80c4a61e170a1c3097377c77c30e9c74336
Sergi Tur Badenas <acacha@gmail.com>
2015-06-07 21:09:55 +0200

    Update htmlheader.blade.php

--- src/resources/views/partials/htmlheader.blade.php
+++ src/resources/views/partials/htmlheader.blade.php
@@ -3,7 +3,7 @@
     <title> AdminLTE 2 with Laravel - @yield('htmlheader_title', 'Your title here') </title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.4 -->
-    <link href="/css/bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
     <!-- Font Awesome Icons -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <!-- Ionicons -->
