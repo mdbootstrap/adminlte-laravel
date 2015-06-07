bd1332586afbc85c63b088af0e0c853302f4f070
Sergi Tur Badenas <acacha@gmail.com>
2015-06-07 21:10:04 +0200

    Update sidebar.blade.php

--- src/resources/views/partials/sidebar.blade.php
+++ src/resources/views/partials/sidebar.blade.php
@@ -7,7 +7,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="{{asset('/img/user2-160x160.jpg'}}" class="img-circle" alt="User Image" />
+                <img src="{{asset('/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image" />
             </div>
             <div class="pull-left info">
                 <p>{{ Auth::user()->name }}</p>
