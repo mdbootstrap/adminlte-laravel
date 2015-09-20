e29278f6322909683088273448875d8c9ef7a3a1
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2015-09-21 00:26:42 +0200

    Solved issue https://github.com/acacha/adminlte-laravel/issues/9

--- src/resources/views/auth/reset.blade.php
+++ src/resources/views/auth/reset.blade.php
@@ -66,12 +66,8 @@
 
     </div><!-- /.login-box -->
 
-    <!-- jQuery 2.1.4 -->
-    <script src="/plugins/jQuery/jQuery-2.1.4.min.js"></script>
-    <!-- Bootstrap 3.3.2 JS -->
-    <script src="/js/bootstrap.min.js" type="text/javascript"></script>
-    <!-- iCheck -->
-    <script src="/plugins/iCheck/icheck.min.js" type="text/javascript"></script>
+    @include('auth.scripts')
+
     <script>
         $(function () {
             $('input').iCheck({
---
+++ src/resources/views/auth/scripts.blade.php
@@ -0,0 +1,6 @@
+<!-- jQuery 2.1.4 -->
+<script src="{{ asset('/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
+<!-- Bootstrap 3.3.2 JS -->
+<script src="{{ asset('/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<!-- iCheck -->
+<script src="{{ asset('/plugins/iCheck/icheck.min.js') }}" type="text/javascript"></script>
