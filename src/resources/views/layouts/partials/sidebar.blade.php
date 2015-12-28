54801be9cacbcbf7787685d818429ab7c86168ae
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2015-12-28 15:27:20 +0100

    Same as last commit but with sidebar

--- src/resources/views/layouts/partials/sidebar.blade.php
+++ src/resources/views/layouts/partials/sidebar.blade.php
@@ -5,16 +5,18 @@
     <section class="sidebar">
 
         <!-- Sidebar user panel (optional) -->
-        <div class="user-panel">
-            <div class="pull-left image">
-                <img src="{{asset('/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image" />
+        @if (! Auth::guest())
+            <div class="user-panel">
+                <div class="pull-left image">
+                    <img src="{{asset('/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image" />
+                </div>
+                <div class="pull-left info">
+                    <p>{{ Auth::user()->name }}</p>
+                    <!-- Status -->
+                    <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+                </div>
             </div>
-            <div class="pull-left info">
-                <p>{{ Auth::user()->name }}</p>
-                <!-- Status -->
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-            </div>
-        </div>
+        @endif
 
         <!-- search form (Optional) -->
         <form action="#" method="get" class="sidebar-form">
