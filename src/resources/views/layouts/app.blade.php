206f997ff750cef73f23514ba2347402228b9586
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2015-12-30 12:17:31 +0100

    Added pratt landing page. v1

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 This is a starter template page. Use this page to start your new project from
 scratch. This page gets rid of all links and provides the needed markup only.
 -->
-<html>
+<html lang="en">
 
 @include('layouts.partials.htmlheader')
 
--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.landing')
 
 @section('content')
 	<div class="container spark-screen">
