54e4ddf0c7d9ecefb5a0bb5bfb41dfd0a8f9a11e
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-01-03 12:50:46 +0100

    Updated plugins to AdminLTE v2.3.2

--- public/plugins/datatables/extensions/Scroller/examples/data/ssp.php
+++ public/plugins/datatables/extensions/Scroller/examples/data/ssp.php
@@ -29,28 +29,30 @@ $primaryKey = 'id';
 // parameter represents the DataTables column identifier. In this case simple
 // indexes
 $columns = array(
-    array('db' => 'id',         'dt' => 0),
-    array('db' => 'firstname',  'dt' => 1),
-    array('db' => 'surname',    'dt' => 2),
-    array('db' => 'zip',        'dt' => 3),
-    array('db' => 'country',    'dt' => 4),
+	array( 'db' => 'id',         'dt' => 0 ),
+	array( 'db' => 'firstname',  'dt' => 1 ),
+	array( 'db' => 'surname',    'dt' => 2 ),
+	array( 'db' => 'zip',        'dt' => 3 ),
+	array( 'db' => 'country',    'dt' => 4 )
 );
 
 // SQL server connection information
 $sql_details = array(
-    'user' => '',
-    'pass' => '',
-    'db' => '',
-    'host' => '',
+	'user' => '',
+	'pass' => '',
+	'db'   => '',
+	'host' => ''
 );
+
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * If you just want to use the basic configuration for DataTables with PHP
  * server-side, there is no need to edit below this line.
  */
 
-require '../../../../examples/server_side/scripts/ssp.class.php';
+require( '../../../../examples/server_side/scripts/ssp.class.php' );
 
 echo json_encode(
-    SSP::simple($_GET, $sql_details, $table, $primaryKey, $columns)
+	SSP::simple( $_GET, $sql_details, $table, $primaryKey, $columns )
 );
+
