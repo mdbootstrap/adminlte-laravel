f442aa3f6fac193ce225d6b95a7ce0346fc619a8
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-02-03 20:21:35 +0100

    landing layout does not have content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,2 @@
 @extends('layouts.landing')
 
-@section('content')
-	<div class="container spark-screen">
-		<div class="row">
-			<div class="col-md-10 col-md-offset-1">
-				<div class="panel panel-default">
-					<div class="panel-heading">Welcome</div>
-
-					<div class="panel-body">
-						Your Application's Landing Page.
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-@endsection
