00f92057d32df2ea0e4b81abdaa94c4249ac31a0
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-03-10 20:35:04 +0100

    SYNC

--- sandbox_destroy.sh
+++ sandbox_destroy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rm -rf sandbox
 
+
--- sandbox_setup.sh
+++ sandbox_setup.sh
@@ -5,11 +5,10 @@ fi
 if ! type "adminlte-laravel" > /dev/null; then
     composer global require "acacha/adminlte-laravel-installer=~1.0"
 fi
-
+rm -rf sandbox
 laravel new sandbox
 cd sandbox
 adminlte-laravel install
 touch database/database.sqlite
-cd ..
 
 
