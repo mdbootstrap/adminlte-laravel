fb8b2a151b074a2f8c2d9ac2151582c4a1b190cf
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-03-17 15:20:30 +0100

    Added Opengraph meta tags and Twitter Cards

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -6,8 +6,22 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Pratt - Free Bootstrap 3 Theme">
-    <meta name="author" content="Alvarez.is - BlackTie.co">
+    <meta name="description" content="Adminlte-laravel - Laravel 5 package that switchs default Laravel scaffolding/boilerplate to AdminLTE template with Bootstrap 3.0 and Pratt Landing page">
+    <meta name="author" content="Sergi Tur Badenas - acacha.org">
+
+    <meta property="og:title" content="Adminlte-laravel" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="Adminlte-laravel - Laravel 5 package that switchs default Laravel scaffolding/boilerplate to AdminLTE template with Bootstrap 3.0 and Pratt Landing page" />
+    <meta property="og:url" content="http://demo.adminlte.acacha.org/" />
+    <meta property="og:image" content="http://demo.adminlte.acacha.org/img/AcachaAdminLTE.png" />
+    <meta property="og:image" content="http://demo.adminlte.acacha.org/img/AcachaAdminLTE600x600.png" />
+    <meta property="og:image" content="http://demo.adminlte.acacha.org/img/AcachaAdminLTE600x314.png" />
+    <meta property="og:sitename" content="demo.adminlte.acacha.org" />
+    <meta property="og:url" content="http://demo.adminlte.acacha.org" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@acachawiki" />
+    <meta name="twitter:creator" content="@acacha1" />
 
     <title>Acacha AdminLTE Laravel package template Landing page - Using Pratt</title>
 
