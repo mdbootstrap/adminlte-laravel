99efd1811707d5cff896415e4516a3370a0d3225
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-04-24 21:36:24 +0530

    user image url fix

--- resources/views/layouts/partials/mainheader.blade.php
+++ resources/views/layouts/partials/mainheader.blade.php
@@ -117,14 +117,14 @@
                         <!-- Menu Toggle Button -->
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <!-- The user image in the navbar-->
-                            <img src="/img/user2-160x160.jpg" class="user-image" alt="User Image"/>
+                            <img src="{{asset('/img/user2-160x160.jpg')}}" class="user-image" alt="User Image"/>
                             <!-- hidden-xs hides the username on small devices so only the image appears. -->
                             <span class="hidden-xs">{{ Auth::user()->name }}</span>
                         </a>
                         <ul class="dropdown-menu">
                             <!-- The user image in the menu -->
                             <li class="user-header">
-                                <img src="/img/user2-160x160.jpg" class="img-circle" alt="User Image" />
+                                <img src="{{asset('/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image" />
                                 <p>
                                     {{ Auth::user()->name }}
                                     <small>Member since Nov. 2012</small>
