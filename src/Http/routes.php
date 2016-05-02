b766b9afd2be996ee068e67fd73bfca818b87db5
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-05-02 14:53:19 +0200

    Welcome route is not necessary as Laravel adds this by default at routes.php file

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,14 +1,14 @@
 <?php
 /*
- * Same configuration as Laravel 5.2:
+ * Same configuration as Laravel 5.2 make auth:
  * See https://github.com/laravel/framework/blob/5.2/src/Illuminate/Auth/Console/stubs/make/routes.stub
+ * but take into account we have to add 'web' middleware group here because Laravel by defaults add this middleware in
+ * RouteServiceProvider
  */
 Route::group(['middleware' => 'web'], function () {
+
     Route::auth();
 
     Route::get('/home', 'HomeController@index');
 
-    Route::get('/', function () {
-        return view('welcome');
-    });
 });
