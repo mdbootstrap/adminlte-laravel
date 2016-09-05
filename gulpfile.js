a5ca1fbf20bb7fada823bf7253384c3d4ec51f3f
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-09-05 10:20:28 +0200

    Added laravel-elixir-vue to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
-var elixir = require('laravel-elixir');
+const elixir = require('laravel-elixir');
+
+require('laravel-elixir-vue');
 
 /*
  |--------------------------------------------------------------------------
