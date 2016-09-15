3f1ad01ce803cab04b10af28ba52432140de0e73
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-09-15 12:12:31 +0200

    Added league flysystem as depedency. Permiet publish command to copy folders like ins done by vendor:publish command in Laravel

--- src/Console/PublishAdminLTE.php
+++ src/Console/PublishAdminLTE.php
@@ -3,12 +3,23 @@
 namespace Acacha\AdminLTETemplateLaravel\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use League\Flysystem\MountManager;
+use League\Flysystem\Filesystem as Flysystem;
+use League\Flysystem\Adapter\Local as LocalAdapter;
 
 /**
  * Class PublishAdminLTE.
  */
 class PublishAdminLTE extends Command
 {
+    /**
+     * The filesystem instance.
+     *
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    protected $files;
+
     /**
      * The name and signature of the console command.
      */
@@ -20,6 +31,18 @@ class PublishAdminLTE extends Command
      * @var string
      */
     protected $description = 'Publish Acacha AdminLTE Template files into laravel project';
+
+    /**
+     * Create a new command instance.
+     *
+     * @param  \Illuminate\Filesystem\Filesystem  $files
+     * @return void
+     */
+    public function __construct(Filesystem $files)
+    {
+        parent::__construct();
+        $this->files = $files;
+    }
 
     /**
      * Execute the console command.
@@ -112,6 +135,13 @@ class PublishAdminLTE extends Command
             if (file_exists($fileDst) && !$this->confirmOverwrite(basename($fileDst))) {
                 return;
             }
+            if ($this->files->isFile($fileSrc)) {
+                $this->publishFile($fileSrc, $fileDst);
+            } elseif ($this->files->isDirectory($fileSrc)) {
+                $this->publishDirectory($fileSrc, $fileSrc);
+            } else {
+                $this->error("Can't locate path: <{$fileSrc}>");
+            }
             copy($fileSrc, $fileDst);
             $this->info('Copied file ' . $fileSrc . ' to ' . $fileDst );
         }
@@ -129,5 +159,71 @@ class PublishAdminLTE extends Command
             ? $fileName.' already exists. Do you want to overwrite it? [y|N]'
             : $prompt;
         return $this->confirm($prompt, false);
+    }
+
+    /**
+     * Create the directory to house the published files if needed.
+     *
+     * @param  string  $directory
+     * @return void
+     */
+    protected function createParentDirectory($directory)
+    {
+        if (! $this->files->isDirectory($directory)) {
+            $this->files->makeDirectory($directory, 0755, true);
+        }
+    }
+
+    /**
+     * Publish the file to the given path.
+     *
+     * @param  string  $from
+     * @param  string  $to
+     * @return void
+     */
+    protected function publishFile($from, $to)
+    {
+        if ($this->files->exists($to) && ! $this->option('force')) {
+            return;
+        }
+        $this->createParentDirectory(dirname($to));
+        $this->files->copy($from, $to);
+        $this->status($from, $to, 'File');
+    }
+
+    /**
+     * Publish the directory to the given directory.
+     *
+     * @param  string  $from
+     * @param  string  $to
+     * @return void
+     */
+    protected function publishDirectory($from, $to)
+    {
+        $manager = new MountManager([
+            'from' => new Flysystem(new LocalAdapter($from)),
+            'to' => new Flysystem(new LocalAdapter($to)),
+        ]);
+        foreach ($manager->listContents('from://', true) as $file) {
+            if ($file['type'] === 'file' && (! $manager->has('to://'.$file['path']) || $this->option('force'))) {
+                $manager->put('to://'.$file['path'], $manager->read('from://'.$file['path']));
+            }
+        }
+        $this->status($from, $to, 'Directory');
+    }
+
+    /**
+     * Write a status message to the console.
+     *
+     * @param  string  $from
+     * @param  string  $to
+     * @param  string  $type
+     * @return void
+     */
+    protected function status($from, $to, $type)
+    {
+        $from = str_replace(base_path(), '', realpath($from));
+        $to = str_replace(base_path(), '', realpath($to));
+        $this->line('<info>Copied '.$type.'</info> <comment>['.$from.']</comment> <info>To</info> <comment>['.$to.']</comment>');
     }
 }
