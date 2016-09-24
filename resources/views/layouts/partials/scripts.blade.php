d7f1a5c8ac4be45fab14001f109cf39b5210b5d3
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-09-24 08:25:00 +0200

    Jquery and Bootstrap js removed because in Laravel 5.3 are added by default

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -1,13 +1,11 @@
 <!-- REQUIRED JS SCRIPTS -->
 
-<!-- jQuery 2.1.4 -->
-<script src="{{ asset('/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
-<!-- Bootstrap 3.3.2 JS -->
-<script src="{{ asset('/js/bootstrap.min.js') }}" type="text/javascript"></script>
-<!-- AdminLTE App -->
-<script src="{{ asset('/js/adminlte-app.min.js') }}" type="text/javascript"></script>
+<!-- JQuery and bootstrap are required by Laravel 5.3 in resources/assets/js/bootstrap.js-->
 <!-- Laravel App -->
 <script src="{{ asset('/js/app.js') }}" type="text/javascript"></script>
+
+<!-- AdminLTE App -->
+<script src="{{ asset('/js/adminlte-app.min.js') }}" type="text/javascript"></script>
 
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
       Both of these plugins are recommended to enhance the
