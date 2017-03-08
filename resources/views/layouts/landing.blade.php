6fb59133181f930a0684b55c4ef69bed63882b2c
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-03-08 14:48:26 +0100

    Use https for fonts

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -28,8 +28,8 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
     <!-- Custom styles for this template -->
     <link href="{{ asset('/css/all-landing.css') }}" rel="stylesheet">
 
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='httpsgit statu://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
 
 </head>
 
