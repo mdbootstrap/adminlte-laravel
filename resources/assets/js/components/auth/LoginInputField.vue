ae0f476b37d1e59fb3332655423524b07404cafc
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-04-10 17:14:48 +0200

    Applied linting to LoginInputField

--- resources/assets/js/components/auth/LoginInputField.vue
+++ resources/assets/js/components/auth/LoginInputField.vue
@@ -7,44 +7,44 @@
 </template>
 
 <script>
-    export default {
-        data: function () {
-          return {
-            credential: ''
-          }
-        },
-        props: {
-          name: {
-            type: String,
-            required: true
-          },
-          domain: {
-            type: String,
-            required: false
-          }
-        },
-        computed: {
-          placeholder: function () {
-            if (this.name === 'email') return this.trans('adminlte_lang_message.email')
-            return this.trans('adminlte_lang_message.username')
-          },
-          type: function () {
-            if (this.name === 'email') return 'email'
-            return 'text'
-          },
-          icon: function () {
-            if (this.name === 'email') return 'glyphicon-envelope'
-            return 'glyphicon-user'
-          }
-        },
-        methods: {
-          adddomain: function () {
-            if (this.type === 'email') return
-            if (this.domain === '') return
-            if (this.credential.endsWith(this.domain)) return
-            if (this.credential.includes('@')) return
-            this.credential = this.credential + '@' + this.domain
-          }
-        }
+export default {
+  data: function () {
+    return {
+      credential: ''
     }
+  },
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    domain: {
+      type: String,
+      required: false
+    }
+  },
+  computed: {
+    placeholder: function () {
+      if (this.name === 'email') return this.trans('adminlte_lang_message.email')
+      return this.trans('adminlte_lang_message.username')
+    },
+    type: function () {
+      if (this.name === 'email') return 'email'
+      return 'text'
+    },
+    icon: function () {
+      if (this.name === 'email') return 'glyphicon-envelope'
+      return 'glyphicon-user'
+    }
+  },
+  methods: {
+    adddomain: function () {
+      if (this.type === 'email') return
+      if (this.domain === '') return
+      if (this.credential.endsWith(this.domain)) return
+      if (this.credential.includes('@')) return
+      this.credential = this.credential + '@' + this.domain
+    }
+  }
+}
 </script>
