4cc6f8d6d1def92452f8aa772075eac98c5ed5c6
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-04-10 17:06:15 +0200

    Removed unnecessay blank file

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   env: {
     browser: true,
     jquery: true,
-
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
