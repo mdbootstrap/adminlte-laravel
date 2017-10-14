2920ec7e553cbd48391f9cdb99427029bdcd0d5e
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-10-14 12:17:48 +0200

    Added some ugly hacks to make AdminLTE 2.4.2 works. I expect to remove then in next versions

--- resources/views/layouts/partials/htmlheader.blade.php
+++ resources/views/layouts/partials/htmlheader.blade.php
@@ -14,6 +14,9 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
     <script>
         //See https://laracasts.com/discuss/channels/vue/use-trans-in-vuejs
         window.trans = @php
