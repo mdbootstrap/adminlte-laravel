056d6ffb47bb102219ca1c78dae1b2e71b368b0d
Manel Gavaldà <manelgavalda1@gmail.com>
2020-07-17 18:19:46 +0200

    Remove dusk registration

--- src/stubs/AppServiceProvider.php
+++ src/stubs/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Laravel\Dusk\DuskServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment('local', 'testing')) {
-            $this->app->register(DuskServiceProvider::class);
-        }
+        //
     }
 }
