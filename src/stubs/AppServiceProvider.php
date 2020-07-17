912a708aef0ef82881ea6902d6ab744ae750e508
Manel Gavaldà <manelgavalda1@gmail.com>
2020-07-17 17:59:03 +0200

    Remove dusk registration

--- src/stubs/AppServiceProvider.php
+++ src/stubs/AppServiceProvider.php
@@ -24,8 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment('local', 'testing')) {
-            $this->app->register(DuskServiceProvider::class);
-        }
+        //
     }
 }
