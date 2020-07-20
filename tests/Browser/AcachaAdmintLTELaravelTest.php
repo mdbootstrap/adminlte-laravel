e1cd4f3d6467d070e525437395b11a48611fb140
Manel Gavaldà <manelgavalda1@gmail.com>
2020-07-20 12:11:09 +0200

    Dusk not showing popup

--- tests/Browser/AcachaAdmintLTELaravelTest.php
+++ tests/Browser/AcachaAdmintLTELaravelTest.php
@@ -380,8 +380,8 @@ class AcachaAdmintLTELaravelTest extends DuskTestCase
             $browser->visit('password/reset')
                 ->type('email', $user->email)
                 ->press('Send Password Reset Link')
-                ->waitFor('div.alert-success')
-                ->assertSeeIn('div.alert-success', 'We have e-mailed your password reset link!');
+                ->pause(1000)
+                ->assertDontSee('We can\'t find a user with that email address.');
         });
     }
 
